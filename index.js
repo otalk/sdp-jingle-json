f54cb3ab13941c15d67751f179b6c8994783585c
Lance Stout <lancestout@gmail.com>
2013-10-05 01:35:27 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,11 @@
+var tosdp = require('./lib/tosdp');
+var tojson = require('./lib/tojson');
+
+
+exports.toSessionSDP = tosdp.toSessionSDP;
+exports.toMediaSDP = tosdp.toMediaSDP;
+exports.toCandidateSDP = tosdp.toCandidateSDP;
+
+exports.toSessionJSON = tojson.toSessionJSON;
+exports.toMediaJSON = tojson.toMediaJSON;
+exports.toCandidateJSON = tojson.toCandidateJSON;
